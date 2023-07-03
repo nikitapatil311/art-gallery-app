023790d6d75d4ca8c2f715c3f4a8c664f493d13b
Nikita <nikitaspatil311@gmail.com>
2023-07-03 21:00:04 +0200

    task 1 initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
+import useSWR from "swr";
+//import ArtPieces from "../components/ArtPieces";
+import Image from "next/image";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art",
+    fetcher
+  );
+  console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <ul>
+        {data.map(({ artist, imageSource, name }) => (
+          <li key={name}>
+            <h1>{name}</h1>
+            <br />
+            <p>
+              Artist: <h2> {artist}</h2>
+            </p>
+
+            <br />
+            <p>Image:</p>
+            <Image src={imageSource} alt={name} width={140} height={230} />
+          </li>
+        ))}
+      </ul>
+
+      {/* <ArtPieces /> */}
     </div>
   );
 }
